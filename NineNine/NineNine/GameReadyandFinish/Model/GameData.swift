1bd8d8ec8fee5ac710935e32c5c495fb12b2fab8
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-07-02 23:47:26 +0900

    Feat: Firestore 연결 및 게임 결과 데이터 저장 구현

--- NineNine/NineNine/GameReadyandFinish/Model/GameData.swift
+++ NineNine/NineNine/GameReadyandFinish/Model/GameData.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
-struct GameStartingData {
+struct GameData {
     private let startingResource: [(String, String, UIImage)] = [
         ("탭탭!", "화면에 있는 치즈 버튼을\n10초동안\n마구마구 누르세요!.", #imageLiteral(resourceName: "TapTapGame_start_cat")),
         ("쉐킷쉐킷!", "휴대폰을 마음껏 흔들어주세요!\n너무 열심히 흔든 나머지\n휴대폰이 날아가지 않도록 주의하세요.", #imageLiteral(resourceName: "ShakeItGame_start_cat")),
@@ -22,10 +23,15 @@ struct GameStartingData {
         ("PlusGame", "PlusGameViewController"),
         ("MultiplyGame", "MultiplyGameViewController")
     ]
+    private let gameTitles: [String] = ["TabTabGame", "ShakeItGame", "BBGame", "PlusGame", "MultiplyGame"]
     private let gameStartBtnImages: [UIImage] = [#imageLiteral(resourceName: "GameStartBtn2"), #imageLiteral(resourceName: "GameStartBtn")]
     
     func gameStartingResource() -> [(String, String, UIImage)] {
         return startingResource
+    }
+    
+    func gameTitleList() -> [String] {
+        return gameTitles
     }
     
     func gameStartBtnImageArray() -> [UIImage] {
@@ -35,4 +41,43 @@ struct GameStartingData {
     func gameStoryBoardAndViewControllers() -> [(String, String)] {
         return gameStoryBoardAndViewControllerList
     }
+}
+
+struct FireStore {
+    private let db = Firestore.firestore()
+    private let uuid = UIDevice.current.identifierForVendor?.uuidString
+    
+    private func makeData(score: Int) -> [String: Any] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM-dd HH:mm:ss"
+        let current_date_string = formatter.string(from: Date())
+        
+        return [
+            "gameTime": current_date_string,
+            "gameScore": score
+        ]
+    }
+    
+    func recordScore(score: Int, gameName: String) {
+        guard let collection_id = uuid else { return }
+        let docRef = db.collection("\(collection_id)").document("\(gameName)")
+        let data: [String: Any] = makeData(score: score)
+        
+        docRef.getDocument { (document, err) in
+            if let document = document, document.exists { // document가 존재하면
+                docRef.updateData(["gameRecord": FieldValue.arrayUnion([data])]) { err in
+                    if let err = err {
+                        print("Error updating document: \(err)")
+                    }
+                }
+            } else { // document가 존재하지 않을 때, 즉 처음 기록하는 경우
+                docRef.setData(["gameRecord": [data]]) { err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    }
+                }
+            }
+        }
+    }
+    
 }
