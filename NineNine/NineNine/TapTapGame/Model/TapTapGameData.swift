29564b6cd43087cad04c9896f18c3613da6ed9ec
{github HEHEEUN} <{heheeun13@gmail.com}>
2023-07-07 17:42:07 +0900

    Feat: 네비게이션 오류 수정

--- NineNine/NineNine/TapTapGame/Model/TapTapGameData.swift
+++ NineNine/NineNine/TapTapGame/Model/TapTapGameData.swift
@@ -5,4 +5,16 @@
 //  Created by 김희은 on 2023/07/05.
 //
 
-import Foundation
+import UIKit
+
+struct TapTapGameData {
+    private let cheeseButtonImage:[UIImage] = [#imageLiteral(resourceName: "TapTapGame_cheesebtn"), #imageLiteral(resourceName: "TapTapGame_cheesebtn_pushed")]
+    private let pushingCatImages:[UIImage] = [#imageLiteral(resourceName: "TapTapGame_cat_before_push"), #imageLiteral(resourceName: "TapTapGame_cat_after_push")]
+    
+    func cheeseButtonImageArray() -> [UIImage] {
+        return cheeseButtonImage
+    }
+    func pushingCatImageArray() -> [UIImage] {
+        return pushingCatImages
+    }
+}
