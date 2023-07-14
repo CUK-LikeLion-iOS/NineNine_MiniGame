33c1413de75b1d8b5ef374364b5fdf875a683ecc
{github HEHEEUN} <{heheeun13@gmail.com}>
2023-07-14 13:28:23 +0900

    Feat: Autolayout 수정

--- NineNine/NineNine/TapTapGame/Model/TapTapGameData.swift
+++ NineNine/NineNine/TapTapGame/Model/TapTapGameData.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct TapTapGameData {
     private let cheeseButtonImage:[UIImage] = [#imageLiteral(resourceName: "TapTapGame_cheesebtn"), #imageLiteral(resourceName: "TapTapGame_cheesebtn_pushed")]
-    private let pushingCatImages:[UIImage] = [#imageLiteral(resourceName: "TapTapGame_cat_before_push"), #imageLiteral(resourceName: "TapTapGame_cat_after_push")]
+    private let pushingCatImages:[UIImage] = [#imageLiteral(resourceName: "TapTapGame_cat_mini_before_push"), #imageLiteral(resourceName: "TapTapGame_cat_mini_after_push")]
     
     func cheeseButtonImageArray() -> [UIImage] {
         return cheeseButtonImage
